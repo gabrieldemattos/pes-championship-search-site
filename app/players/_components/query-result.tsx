a99613ff34c8c7ae0db31a45872fd23e95b24f33
Gabriel de Mattos <mattos_95@hotmail.com>
2024-12-23 18:37:53 -0300

    refactor: refactor code

---
+++ app/players/_components/query-result.tsx
@@ -0,0 +1,76 @@
+import ErrorMessage from "@/app/_components/error-message";
+import SearchPlayer from "@/app/_components/search-player";
+import { Button } from "@/app/_components/ui/button";
+import { positionColors } from "@/app/_constants/position-colors";
+import { Players } from "@prisma/client";
+import Link from "next/link";
+
+interface PlayersProps {
+  players: Players[];
+  isLoading: boolean;
+  searchFor: string;
+  error: string;
+}
+
+const QueryResult = ({
+  players,
+  isLoading,
+  searchFor,
+  error,
+}: PlayersProps) => {
+  return (
+    <div className="pb-10 p-5">
+      <div className="mt-8">
+        <SearchPlayer isLoading={isLoading} />
+      </div>
+
+      <div className="mt-10">
+        <p className="text-lg">
+          Resultados para:{" "}
+          <span className="font-bold italic capitalize">
+            {searchFor?.toLocaleLowerCase()}
+          </span>
+        </p>
+      </div>
+
+      <div className="mt-5">
+        {isLoading ? (
+          <p className="text-white text-lg">Buscando jogador...</p>
+        ) : (
+          <div className="space-y-3">
+            {players.map((player) => (
+              <Button
+                key={player.id}
+                className="flex justify-between border text-base p-2 font-semibold rounded bg-transparent"
+                asChild
+              >
+                <Link href={`/players/${player.id}`}>
+                  <p className="capitalize">
+                    {player.name} ({player.playerOwner})
+                  </p>
+                  <p
+                    className={`uppercase border-b-2 ${
+                      positionColors[player.mainPosition]
+                    }`}
+                  >
+                    {player.mainPosition}
+                  </p>
+                </Link>
+              </Button>
+            ))}
+          </div>
+        )}
+
+        {players.length === 0 && !isLoading && error === "" && (
+          <p className="text-white text-base opacity-80 mt-10">
+            Nenhum jogador encontrado com esse nome..
+          </p>
+        )}
+
+        {error !== "" && !isLoading && <ErrorMessage error={error} />}
+      </div>
+    </div>
+  );
+};
+
+export default QueryResult;
--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { Button } from "../_components/ui/button";
 import { Suspense, useEffect, useState } from "react";
 import { Players } from "@prisma/client";
 import { searchPlayers } from "./_actions/search";
-import Link from "next/link";
-import { positionColors } from "../_constants/position-colors";
-import SearchPlayer from "../_components/search-player";
-import ErrorMessage from "../_components/error-message";
+import QueryResult from "./_components/query-result";
 
 const PlayersPage = () => {
   const searchParams = useSearchParams();
@@ -43,59 +39,16 @@ const PlayersPage = () => {
     fetchPlayers();
   }, [searchFor]);
 
+  if (!searchFor) return;
+
   return (
     <Suspense>
-      <div className="pb-10 p-5">
-        <div className="mt-8">
-          <SearchPlayer isLoading={isLoading} />
-        </div>
-
-        <div className="mt-10">
-          <p className="text-lg">
-            Resultados para:{" "}
-            <span className="font-bold italic capitalize">
-              {searchFor?.toLocaleLowerCase()}
-            </span>
-          </p>
-        </div>
-
-        <div className="mt-5">
-          {isLoading ? (
-            <p className="text-white text-lg">Buscando jogador...</p>
-          ) : (
-            <div className="space-y-3">
-              {players.map((player) => (
-                <Button
-                  key={player.id}
-                  className="flex justify-between border text-base p-2 font-semibold rounded bg-transparent"
-                  asChild
-                >
-                  <Link href={`/players/${player.id}`}>
-                    <p className="capitalize">
-                      {player.name} ({player.playerOwner})
-                    </p>
-                    <p
-                      className={`uppercase border-b-2 ${
-                        positionColors[player.mainPosition]
-                      }`}
-                    >
-                      {player.mainPosition}
-                    </p>
-                  </Link>
-                </Button>
-              ))}
-            </div>
-          )}
-
-          {players.length === 0 && !isLoading && error === "" && (
-            <p className="text-white text-base opacity-80 mt-10">
-              Nenhum jogador encontrado com esse nome..
-            </p>
-          )}
-
-          {error !== "" && !isLoading && <ErrorMessage error={error} />}
-        </div>
-      </div>
+      <QueryResult
+        players={players}
+        isLoading={isLoading}
+        searchFor={searchFor}
+        error={error}
+      />
     </Suspense>
   );
 };
