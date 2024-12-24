fa128578d55cfc9ca3e196b1f27f8ab95a5c0f40
Gabriel de Mattos <mattos_95@hotmail.com>
2024-12-24 16:34:48 -0300

    style: adjust styles responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SearchPlayer from "./_components/search-player";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen text-white flex flex-col pt-3">
+    <div className="w-screen h-screen text-white flex flex-col pt-3 fixed top-0 left-0 translate-y-[15%]">
       <div className="relative h-[250px] w-full mt-10 md:h-[350px]">
         <Image
           src="/pes-logo.png"
