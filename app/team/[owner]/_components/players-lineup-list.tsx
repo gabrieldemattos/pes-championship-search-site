d06b6522c3424c008bfce6ba7e74cb3f720532b0
Gabriel de Mattos <mattos_95@hotmail.com>
2025-12-19 01:49:31 -0300

    fix: scrollbar

--- app/team/[owner]/_components/players-lineup-list.tsx
+++ app/team/[owner]/_components/players-lineup-list.tsx
@@ -3,7 +3,7 @@ import PlayerLineupItem from "./player-lineup-item";
 
 const PlayersLineupList = ({ players }: { players: Players[] }) => {
   return (
-    <div className="flex gap-3 overflow-x-scroll pb-5 text-black">
+    <div className="flex gap-3 overflow-x-scroll pb-5 text-black [&::-webkit-scrollbar]:h-4 [&::-webkit-scrollbar-thumb]:rounded-md [&::-webkit-scrollbar-thumb]:bg-gray-700 [&::-webkit-scrollbar-track]:bg-gray-400 [&::-webkit-scrollbar-track]:rounded-md">
       {players.map((p) => (
         <PlayerLineupItem key={p.id} player={p} />
       ))}
