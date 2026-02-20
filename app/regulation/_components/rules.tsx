a29efa8ac2f338e15f010e0222ab17ff3e87dee1
Gabriel de Mattos <mattos_95@hotmail.com>
2026-02-20 15:17:59 -0300

    styles: add new image

--- app/regulation/_components/rules.tsx
+++
@@ -1,9 +0,0 @@
-const Rules = ({ rule }: { rule: string }) => {
-  return (
-    <li className="text-lg text-gray-300">
-      <span className="font-semibold italic">{rule}</span>
-    </li>
-  );
-};
-
-export default Rules;
